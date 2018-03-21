d1296893f6ff09a8f7e001fa701fee1eae9560b1
Cafer Kaya <cafer.kaya@yemeksepeti.com>
2018-03-21 15:59:56 +0300

    Add new layout to test fragment

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/FragmentTest.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/FragmentTest.java
@@ -33,6 +33,6 @@ public class FragmentTest extends DaggerFragment {
 
   @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.addtask_frag, container, false);
+    return inflater.inflate(R.layout.test_frag, container, false);
   }
 }
